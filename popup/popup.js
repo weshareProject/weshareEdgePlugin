53acaf7a955fbd01775f809a9cbdd695f688e422
hocyi <hocyisemail@qq.com>
2023-10-31 21:30:58 +0800

    空白设置页面 对popup略作修改

--- popup/popup.js
+++ popup/popup.js
@@ -8,52 +8,12 @@ async function getCurrentTab() {
   return tab;
 }
 
-getCurrentTab().then(v=>{$("url").innerHTML=v['url'];});
-
-let highlightmode=0;
-
-/*
-chrome.runtime.sendMessage({op:"get",key:"highlightmode"},response=>{
-	if(!response)highlightmode=0;
-	else highlightmode=response;
-	if(highlightmode==1){
-		$("bt").value="标记已开启";
-	}else{
-		$("bt").value="标记已关闭";
-	}
-});
-*/
-
-/*
-async function load(){
-	highlightmode=await chrome.storage.sync.get("highlightmode");
-	if(!highlightmode)highlightmode=0;
-	else highlightmode=highlightmode["highlightmode"];
-	if(highlightmode==1){
-		$("bt").value="标记已开启";
-	}else{
-		$("bt").value="标记已关闭";
-	}
-}
-
-load();
+(async ()=>{
+	let v=await getCurrentTab()
+	$('url').innerHTML=v['url'];
+})();
 
 
-$('bt').addEventListener("click",()=>{
-	if(highlightmode==1){
-		$("bt").value="标记已关闭";
-		highlightmode=0;
-	}else{
-		$("bt").value="标记已开启";
-		highlightmode=1;
-	}
-	
-	chrome.storage.sync.set({"highlightmode":highlightmode});
-	
-	//chrome.runtime.sendMessage({op:"set",key:"highlightmode",value:highlightmode});
-});
-
-*/
 
 $('nl').addEventListener("click",()=>{
 	
@@ -61,4 +21,8 @@ $('nl').addEventListener("click",()=>{
 		
 		chrome.tabs.sendMessage(response[0].id,{op:"addNote",position:"default"});
 	});
+});
+
+$('setting').addEventListener('click',()=>{
+	chrome.tabs.create({url:"/option/option.html"});
 });
